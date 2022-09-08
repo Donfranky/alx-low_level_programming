25d7e6c4b8eb8970a318f95968e955ef675c8694
Franklin Sopulu Eze <ezefranklin141516@gmail.com>
2022-09-08 16:32:32 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
 #!/bin/bash
 #include <stdio.h>
+
 /**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a mutilingual puzzle
+ * Return: 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
