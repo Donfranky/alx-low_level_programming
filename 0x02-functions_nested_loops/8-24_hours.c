01df1ac735db164fa9ef55edb919507ebf57b402
Franklin Sopulu Eze <ezefranklin141516@gmail.com>
2022-09-14 15:37:40 -0700

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,6 +11,13 @@ int h, m;
 for (h = 0; h < 24; h++)
 	{
 	for (m = 0; m < 60; m++)
+		{
+		_putchar((h / 10) + '0');
+		_putchar((h % 10) + '0');
+		_putchar(':');
+		_putchar((m / 10) + '0');
+		_putchar((m % 10) + '0');
+		_putchar('\n');
+		}
 	}
-_putchar((h / 10) + '0');
 }
