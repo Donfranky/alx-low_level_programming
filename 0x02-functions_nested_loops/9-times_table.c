32b81bd6436e60df8b3d997434b9a89a5239fc3a
Franklin Sopulu Eze <ezefranklin141516@gmail.com>
2022-09-14 16:12:44 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,59 +5,25 @@
  */
 void times_table(void)
 {
-	int i, j, u, d, result;
+	int num, mult, prod;
 
-	for (i = 0; i <= 9; i++)
+	for (num = 0; num <= 9; num++)
 	{
-		for (j = 0; j <= 9; j++)
-		{
-<<<<<<< HEAD
-			if(x > 0)
-			{
- 				_putchar(',');
-			}
-			result = y * x;
-			if(result <= 9)
-			{
-				dig1 = result;
- 				_putchar('0' + dig1);
-			}
-			if(result > 9)
-			{
-				dig1 = result / 10;
-				dig2 = result % 10;
-				_putchar('0' + dig1);
- 				_putchar('0' + dig2);
-=======
-			result = i * j;
-			d = result / 10;
-			u = result % 10;
+		_putchar('0');
 
-			if (result == 0)
-			{
-				_putchar('0' + u);
-			}
-			if (i == 0 && j < 9)
-			{
-				_putchar(',');
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod + num * mult;
+
+			if(prod <= 9)
 				_putchar(' ');
-				_putchar(' ');
-			}
-			if (result > 0 && result <= 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + u);
-			}
-			if (result > 9)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar('0' + d);
-				_putchar('0' + u);
->>>>>>> bcbbdddacb50fc929e423b95e635ca0231cccbdd
-			}
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 	}
