5fa2cef0339dd35db63e48a4517e5a496299c2ec
Franklin Sopulu Eze <ezefranklin141516@gmail.com>
2022-09-14 15:20:19 -0700

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * jack_bauer - function that prints every minute of the day ofJack Bauer
+ * h = hour, m = minutes
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+
+int h, m;
+for (h = 0; h < 24; h++)
+	{
+	for (m = 0; m < 60; m++)
+	}
+_putchar((h / 10) + '0');
+}
