2123b0514d7c586750b6ea2a45b5d964ef44a9bc
Franklin Sopulu Eze <ezefranklin141516@gmail.com>
2022-10-04 17:18:11 -0700

    0.Create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,27 +9,19 @@
  * pointer to array if everything is normal.
  */
 
-char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c)
 {
-	char *s;
-	unsigned int i;
+	char *array;
+        unsigned int index;
 
-	if (size == 0)
-	{
-		return (NULL);
-	}
+        if (size == 0)
+                return (NULL);
+        array = malloc(sizeof(char) * size);
 
-	s = malloc((size) * sizeof(char));
-	if (s == NULL)
-	{
-		return (NULL);
-	}
-	i = 0;
-	while (i < size)
-	{
-		s[i] = c;
-		i++;
-	}
-	s[i] = '\0';
-	return (s);
+        if (array == NULL)
+                return (NULL);
+
+        for (index = 0; index < size; index++)
+                array[index] = c;
+        return (array);
 }
